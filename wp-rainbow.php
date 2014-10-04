d1bfcc3996b079ea025752fecf091777a902c94c
mcguffin <joern@flyingletters.com>
2014-10-04 11:55:41 +0200

    obfuscate email

--- wp-rainbow.php
+++ wp-rainbow.php
@@ -13,7 +13,7 @@ Text Domain: wp-rainbow-hilite
 Domain Path: /languages/
 */
 
-/*  Copyright 2014  Jörn Lund  (email : joern@podpirate.org)
+/*  Copyright 2014  Jörn Lund  (email : joern AT podpirate DOT org)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
