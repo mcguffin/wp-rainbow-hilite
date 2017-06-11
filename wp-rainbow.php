2a63a89c75a2313e67a004d8d0418fd4f1c1d420
mcguffin <joern@flyingletters.com>
2017-06-11 16:24:37 +0200

    alpha

--- wp-rainbow.php
+++ wp-rainbow.php
@@ -5,7 +5,7 @@ Plugin Name: WordPress Rainbow Hilite
 Plugin URI: http://wordpress.org/plugins/wp-rainbow-hilite/
 Description: Code Syntax coloring using <a href="http://craig.is/making/rainbows">rainbow</a>.
 Author: Jörn Lund
-Version: 2.0.0
+Version: 2.0.0a
 Author URI: https://github.com/mcguffin
 License: GPL3
 
